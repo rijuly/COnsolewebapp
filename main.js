619798bf810b877f3c57e13bfba1639e2f64dd52
rijuly <136062565+rijuly@users.noreply.github.com>
2023-08-08 17:27:44 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var fname="Ja";
+console.log("firstname:"+fname);
+var lname="Morant";
+console.log("lastname:"+lname);
+var fullname= fname.concat(lname)
+console.log("fullname:"+fullname);
+var lenght_fullname=fullname.length;
+console.log("length of fullname:"+lenght_fullname);
+var new_name=fullname.replace("Ja","Jaron");
+console.log("newname:"+new_name);
+var game="BASKETBALL";
+console.log("game:"+game);
+var game_lower=game.toLowerCase();
+console.log("gamelower:"+game_lower);
+var num1=10;
+console.log("num1:"+num1);
+var num2=11;
+console.log("num2:"+num2);
+var total=num1+num2
+console.log("addition:"+total);
+var sub=num2-num1
+console.log("subtract:"+sub);
+var div=10/2
+console.log("division 10 by 2:"+div);
+var mul=num2*num1
+console.log("=multiplaction:"+mul);
